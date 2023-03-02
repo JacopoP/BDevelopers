4c3665f706c6ccf302ecb96bfa7d1a4d3d3e2cea
Jac22 <jacopo.prudentino@gmail.com>
2023-03-02 18:20:12 +0100

    Added UserSeeder to DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
+            UserSeeder::class,
             DeveloperSeeder::class,
             RatingSeeder::class,
             SponsorSeeder::class,
