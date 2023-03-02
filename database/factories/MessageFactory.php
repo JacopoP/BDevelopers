f1f906d15661b5d12a3d47b4e45f777fa51dc909
Francesco Limpias <francescolimpias@gmail.com>
2023-03-02 16:54:43 +0100

    Created Message factory

---
+++ database/factories/MessageFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Message>
+ */
+class MessageFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'text' => fake()->sentences(5, true),
+            'email' => fake()->safeEmail(),
+            'full_name' => fake()->name(),
+            'date_sent' => fake()->dateTimeBetween('-24 week'),
+            'seen' => fake()->boolean(80),
+        ];
+    }
+}
