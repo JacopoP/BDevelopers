d618039cd9f88917de0ceec13c6b1b5c6ed82c5e
Francesco Limpias <francescolimpias@gmail.com>
2023-03-09 16:50:37 +0100

    Added better user seeding logic - emails match user names - added emailForNames faker

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
@@ -17,10 +18,21 @@ class UserFactory extends Factory
      */
     public function definition()
     {
+        // Store in array first and last name
+        $name = fake()->firstName();
+        $last = fake()->lastName();
+
+        $email = '';
+        $failed = 0;
+        do {
+            $email = $this::emailForNames($name, $last . ($failed ? fake()->year() : ''));
+            $failed++;
+        } while (User::where('email', $email)->get()->count());
+
         return [
-            'name' => fake()->firstName(),
-            'last' => fake()-> lastName(),
-            'email' => fake()->unique()->safeEmail(),
+            'name' => $name,
+            'last' => $last,
+            'email' => $email,
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
@@ -37,5 +49,36 @@ class UserFactory extends Factory
         return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
         ]);
+    }
+
+
+    /**
+     * Generates fake email with given names
+     *
+     * @return string
+     */
+    public static function emailForNames($name, $last)
+    {
+        // Allowed chars
+        $regex = '/[^A-Za-z0-9\-]/';
+
+        // Store in array first and last name
+        $firstLast = [
+            preg_replace($regex, '', $name), 
+            preg_replace($regex, '', $last)
+        ];
+
+        // Random decide how to compose mail
+        $emailNameFirst = fake()->boolean();
+
+        // Generate mail
+        $email = $firstLast[$emailNameFirst]
+            // second name
+            . '.' . $firstLast[!$emailNameFirst]
+            // email domain
+            . '@' . fake()->freeEmailDomain();
+        $email = strtolower($email);
+
+        return $email;
     }
 }
