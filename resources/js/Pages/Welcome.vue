77353d8784e4b0ef5b456e508697b006c5fb3b50
Francesco Limpias <francescolimpias@gmail.com>
2023-03-13 17:41:07 +0100

    Added base HTML layout

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -12,9 +12,30 @@ defineProps({
 </script>
 
 <template>
-    <Head title="Welcome" />
-    <GuestNavbar :canLogin="canLogin" :canRegister="canRegister" :laravelVersion="laravelVersion" :phpVersion="phpVersion"/>
+    <!-- Meta -->
+    <Head title="Landing" />
+
+    <!-- default view -->
+    <!-- <GuestNavbar :canLogin="canLogin" :canRegister="canRegister" :laravelVersion="laravelVersion" :phpVersion="phpVersion"/> -->
+
+    <div id="LandingPage">
+        <h1>
+            Landing Page
+        </h1>
+    </div>
 </template>
+
+<style lang="scss">
+#LandingPage{
+
+    // Full viewport size
+    height: 100vh;
+
+    // Defaults
+    background-color: #1d1d1d;
+    color: snow;
+}
+</style>
 
 
 
