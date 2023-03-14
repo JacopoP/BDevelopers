1e7b468a9a13e3efa730cff99a64db4d2dfdafbc
Francesco Limpias <francescolimpias@gmail.com>
2023-03-14 10:29:30 +0100

    Base vue

--- resources/js/Pages/Index.vue
+++ resources/js/Pages/Index.vue
@@ -1,88 +1,87 @@
 <script>
-import axios from 'axios';
-const apiUrl = 'http://localhost:8000/api/';
-const apiVersion = 'v1/';
-const api = apiUrl + apiVersion;
 export default {
-    props: ['technologies'],
+    name: 'ResultsPage',
     data() {
         return {
-            developers: Object,
-            nameFilter: '',
-            post: {
-                nFilter: '',
-                lFilter: '',
-                ratingFilter: null,
-                reviewFilter: null,
-                techFilter: [],
-            }
-        }
-    },
-    methods: {
-        goFilter: function () {
-            let textArray = this.nameFilter.split(' ');
-            this.post.nFilter = textArray[0];
-            if (textArray.length >= 1) { textArray.shift() };
-            this.post.lFilter = textArray.join(' ');
-
-            axios.post(api + 'search', this.post)
-                .then((res) => {
-                    this.developers = res.data.response.developers
-                })
-                .catch((err) => console.log(err));
-        }
-    },
-    mounted() {
-        axios.get(api + 'index')
-            .then((res) => {
-                this.developers = res.data.response.developers
-            })
-            .catch((err) => console.log(err));
+            developers: [
+                { name: 'John Doe', skills: ['JavaScript', 'Vue.js'], rating: 4, reviews: 25 },
+                { name: 'Jane Smith', skills: ['HTML', 'CSS', 'Bootstrap'], rating: 5, reviews: 10 },
+                { name: 'Bob Johnson', skills: ['React', 'Node.js'], rating: 3, reviews: 5 },
+                { name: 'Alice Lee', skills: ['PHP', 'WordPress'], rating: 4, reviews: 3 },
+            ]
+        };
     }
 }
 </script>
 
 <template>
-    <form @submit.prevent="goFilter">
-        <h2>Filters</h2>
-        <label for="nameFilter">Name</label>
-        <input type="text" v-model="this.nameFilter" name="nameFilter" placeholder="name">
-        <label for="ratingFilter">Raitings avarage</label>
-        <select v-model="this.post.ratingFilter" id="raitingFilter">
-            <option value="0">Reset</option>
-            <option value="1">1+</option>
-            <option value="2">2+</option>
-            <option value="3">3+</option>
-            <option value="4">4+</option>
-        </select>
-        <label for="reviewFilter">Review number</label>
-        <select v-model="this.post.reviewFilter" id="raitingFilter">
-            <option value="0">Reset</option>
-            <option value="5">5+</option>
-            <option value="10">10+</option>
-            <option value="50">50+</option>
-            <option value="100">100+</option>
-        </select>
-        <h2>Technology filter</h2>
-        <div v-for="technology in this.technologies">
-            <input type="checkbox" v-model="this.post.techFilter" :value="technology.id">
-            <label for="techFilter[]">{{ technology.name }}</label>
-        </div>
-        <input type="submit" value="Filter">
-    </form>
-    <br>
-    <ul>
-        <li v-for="developer in this.developers">
-            {{ developer.user.name }} {{ developer.user.last }} <br>
-            {{ developer.user.email }} <br>
-            <span v-for="tech in developer.technologies">{{ tech.name }}&nbsp;</span>
-            <hr>
-        </li>
-    </ul>
+    <div class="results-page">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+            <div class="container">
+                <a class="navbar-brand" href="#">BDevelopers</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Login</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <main>
+            <div class="filters">
+                <div class="container">
+                    <div class="row justify-content-between">
+                        <div class="col-md-4 mb-3 mb-md-0">
+                            <div class="form-group">
+                                <label for="min-rating">Minimum rating:</label>
+                                <select class="form-control" id="min-rating" v-model="minRating">
+                                    <option value="">Any</option>
+                                    <option v-for="n in 5" :value="n" :key="n">{{ n }} star{{ n !== 1 ? 's' : '' }}</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <label for="min-reviews">Minimum reviews:</label>
+                                <select class="form-control" id="min-reviews" v-model="minReviews">
+                                    <option value="">Any</option>
+                                    <option v-for="n in 10" :value="n" :key="n">{{ n }}+</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="results">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-4" v-for="(developer, index) in filteredDevelopers" :key="index">
+                            <div class="card mb-4">
+                                <div class="card-header">{{ developer.name }}</div>
+                                <div class="card-body">
+                                    <div class="mb-2"><strong>Skills:</strong> {{ developer.skills.join(', ') }}</div>
+                                    <div class="mb-2"><strong>Rating:</strong> {{ developer.rating }} stars</div>
+                                    <div class="mb-2"><strong>Reviews:</strong> {{ developer.reviews }} reviews</div>
+                                    <button class="btn btn-primary">Hire {{ developer.name }}</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+    </div>
 </template>
 
-<style lang="scss">
-ul {
-    list-style: none;
+<style>
+main {
+    padding-top: 80px;
+    /* adjust this value to match the height of your navbar */
 }
 </style>
