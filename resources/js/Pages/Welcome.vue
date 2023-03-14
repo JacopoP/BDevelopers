f296cf0bfff714fb766d408f4a29cbee1cf3b616
Francesco Limpias <francescolimpias@gmail.com>
2023-03-14 10:44:26 +0100

    Added welcome.vue graphics

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,10 +1,12 @@
 <script>
 import { Head } from '@inertiajs/vue3';
 import GuestNavbar from '@/Components/WelcomeHead/Navbar.vue';
-import {Link} from '@inertiajs/vue3';
+import { Link } from '@inertiajs/vue3';
 
-export default{
-    components:{
+export default {
+    name: 'LandingPage',
+
+    components: {
         Head,
         GuestNavbar,
         Link,
@@ -17,18 +19,18 @@ export default{
         technologies: Array,
         developers: Object,
     },
-    data(){
-        return{
-            form:{
+    data() {
+        return {
+            form: {
                 name: '',
                 tech: null,
             }
         }
     },
-    methods:{
-        goToIndex: function(){
+    methods: {
+        goToIndex: function () {
             axios.get('http://localhost:8000/index', this.form)
-            .then((res)=>console.log(res));
+                .then((res) => console.log(res));
         }
     }
 }
@@ -36,13 +38,88 @@ export default{
 
 <template>
     <Head title="Welcome" />
-    <GuestNavbar :canLogin="canLogin" :canRegister="canRegister" :laravelVersion="laravelVersion" :phpVersion="phpVersion"/>
-        <input type="text" v-model="form.name">
-        <select v-model="form.tech">
-            <option v-for="tech in technologies" :value="tech.id">{{ tech.name }}</option>
-        </select>
-        <Link href="/index" method="post" :data="this.form" as="button"> Start searching </Link>            
+    <div class="landing-page">
+
+        <!-- NavBar -->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+            <div class="container">
+
+                <!-- BDevelopers logo + title -->
+                <a class="navbar-brand" href="#">BDevelopers</a>
+
+                <!-- Nav -->
+                <div id="navbarNav">
+                    <ul class="navbar-nav ml-auto list-unstyled d-flex flex-row gap-2">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/login">Login</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/register">Register</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <!-- HeroSection -->
+        <div class="hero-section">
+            <div class="container">
+                <div class="row align-items-center justify-content-center" style="height: 100vh;">
+                    <div class="col-md-6">
+                        <h1 class="text-center mb-4">Start your research</h1>
+                        <form class="form-inline d-flex justify-content-center">
+                            <div class="form-group d-flex">
+
+                                <input type="text" class="form-control form-control-lg mr-3" placeholder="Search..."
+                                    v-model="form.name">
+                                <select v-model="form.tech" class="text-dark">
+                                    <option class="text-dark" v-for="tech in technologies" :value="tech.id">{{ tech.name }}</option>
+                                </select>
+                                <Link href="/index" method="post" :data="this.form" as="button"
+                                    class="btn btn-lg btn-primary"> Go </Link>
+
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Sponsor -->
+        <!-- work in progress -->
+
+    </div>
 </template>
+  
+<style lang="scss">
+// Import Bootstrap SCSS
+//   @import '~bootstrap/scss/bootstrap';
+
+// Define custom styles for the landing page
+.landing-page {
+    .hero-section {
+        background-color: #424242;
+        color: #fff;
+
+        h1 {
+            font-size: 3rem;
+            font-weight: bold;
+        }
+
+        form {
+            button {
+                background-color: #ff6769;
+                border-color: #ff6769;
+
+                &:hover {
+                    background-color: darken(#ff6769, 10%);
+                    border-color: darken(#ff6769, 10%);
+                }
+            }
+        }
+    }
+}
+</style>
 
 
 
