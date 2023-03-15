830437a78aae0a28d2573b5e0e8307300b10f60e
Jac22 <jacopo.prudentino@gmail.com>
2023-03-15 16:25:12 +0100

    Added client side braintree

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,6 +9,7 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <script src="https://js.braintreegateway.com/web/dropin/1.36.0/js/dropin.js"></script>
 
         <!-- Scripts -->
         @routes
