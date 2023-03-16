2b09d7e07dc74251ba598c1b246d3c411e032ab5
Jac22 <jacopo.prudentino@gmail.com>
2023-03-16 18:08:53 +0100

    Given layout to Payment page

--- resources/js/Pages/Braintree/PaymentPage.vue
+++ resources/js/Pages/Braintree/PaymentPage.vue
@@ -1,19 +1,25 @@
 <script setup>
+import GuestLayout from '@/Layouts/GuestLayout.vue';
 import { useForm } from '@inertiajs/vue3';
 import { onMounted } from 'vue';
-    const form = useForm({
-        payment_method_nonce: undefined,
-        amount: 0,
-    });
-let prova = function(){};
-const props = defineProps(['token']);
+const form = useForm({
+    payment_method_nonce: undefined,
+    amount: 0,
+});
+let doPayment = function(){};
+function paymentCheck(){
+    if(form.amount){
+        doPayment();
+    }
+}
+const props = defineProps(['token', 'sponsors']);
 onMounted(()=>{
     braintree.dropin.create({
             authorization: props.token,
-        selector: '#dropin-container'
+            selector: '#dropin-container'
         }, (err, dropinInstance) => {
             if (err) console.error(err);
-            prova = function(){
+            doPayment = function(){
                 dropinInstance.requestPaymentMethod((error, payload) => {
                     if (error) console.error(error);
                     form.payment_method_nonce = payload.nonce;
@@ -24,22 +30,44 @@ onMounted(()=>{
 })
 </script>
 
+<script>
+export default{
+    data(){
+        return{
+            isProcessing: false,
+            clicked: false,
+        }
+    },
+}
+</script>
+
 <template>
-    <form id="payment-form" method="post" @submit.prevent="prova">
-        <div id="dropin-container"></div>
-        <input type="radio" value="2.99" v-model="form.amount" id="plan1">
-        <label for="plan1">Base</label>
-        <br>
-        <input type="radio" value="5.99" v-model="form.amount" id="plan2">
-        <label for="plan2">Intermediate</label>
-        <br>
-        <input type="radio" value="9.99" v-model="form.amount" id="plan3">
-        <label for="plan2">Advanced</label>
-        <br>
-        <input type="submit" />
-    </form>
+    <GuestLayout class="bg-dark text-light overflow-hidden">
+        <form id="payment-form" method="post" @submit.prevent="()=>{paymentCheck(); isProcessing=true}">
+            <div id="dropin-container"></div>
+            <div class="d-flex justify-content-between gap-3 mt-4">
+                <div :class="'card bg-dark border-light text-center' + [form.amount == sponsor.price ? '' : ' opacity-50']" v-for="sponsor in sponsors" @click="()=>{form.amount = sponsor.price}">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ sponsor.plan_name }}</h5>
+                        <p class="card-text">
+                            Sponsor duration: {{ sponsor.length }} hours <br>
+                            Price: &euro; {{ sponsor.price }}
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <input class="btn btn-light mt-4 mx-auto d-block" type="submit" value="Pay and get sponsored" @click="this.clicked=true" :disabled="isProcessing"/>
+            <p v-show="form.amount == 0 && this.clicked" class="text-center text-danger mt-3">Please, select a plan before checking out</p>
+        </form>
+    </GuestLayout>
 </template>
 
 <style lang="scss" scoped>
-
+.card{
+    cursor: pointer;
+    transition: transform .2s;
+    &:hover{
+        transform: scale(1.1);
+    }
+}
 </style>
--- resources/js/Pages/Braintree/Result.vue
+++ resources/js/Pages/Braintree/Result.vue
@@ -1,9 +1,16 @@
 <script setup>
+import GuestLayout from '@/Layouts/GuestLayout.vue';
 const props = defineProps(['result', 'developer']);
 console.log(props.result);
 console.log(props.developer);
 </script>
 
 <template>
-
+    <GuestLayout>
+        <div class="my_container bg-dark">
+            <template v-if="result.success">
+                <h1>Congratulation!</h1>
+            </template>
+        </div>
+    </GuestLayout>
 </template>
