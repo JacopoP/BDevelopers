8b0cbe1a226369c2781796a180a461914c9b4737
Vincenzo <vincenzo.foti@outlook.it>
2023-03-16 17:08:33 +0100

    fixed page height

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -4,23 +4,23 @@ import Navbar from '@/Components/DashboardHead/Navbar.vue';
 </script>
 
 <template>
-    <div>
-        <div class="pb-5 bg-dark">
-            <Navbar/>
-            <!-- SubNavbar -->
-            <header class="bg-dark text-light shadow" v-if="$slots.header">
-                <div class="container py-2">
-                    <slot name="header" />
-                </div>
-            </header>
+
+    <div class="pb-5 bg-dark" style="min-height: 100vh;">
+        <Navbar/>
+        <!-- SubNavbar -->
+        <header class="bg-dark text-light shadow" v-if="$slots.header">
+            <div class="container py-2">
+                <slot name="header" />
+            </div>
+        </header>
 
 
 
-            <!-- Page Content -->
-            <main>
-                <slot name="main" />
-            </main>
+        <!-- Page Content -->
+        <main>
+            <slot name="main" />
+        </main>
 
-        </div>
     </div>
+
 </template>
