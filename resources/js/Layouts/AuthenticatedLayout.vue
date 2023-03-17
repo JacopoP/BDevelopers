cee91790a8677142b3814f41ee8207fa4a319cf1
Jac22 <jacopo.prudentino@gmail.com>
2023-03-17 11:25:42 +0100

    Solved some AutenticatedLayout issues

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -4,24 +4,21 @@ import Navbar from '@/Components/DashboardHead/Navbar.vue';
 </script>
 
 <template>
-    <div>
-        <div class="bg-dark" style="min-height: 100vh;">
-            <!-- <div class="pb-5 bg-dark"> -->
-            <Navbar />
-            <!-- SubNavbar -->
-            <header class="bg-dark text-light shadow py-3" style="margin-top: 60px;" v-if="$slots.header">
-                <div class="container py-6 px-4">
-                    <slot name="header" />
-                </div>
-            </header>
+    <div class="bg-dark" style="min-height: 100vh;">
+        <!-- <div class="pb-5 bg-dark"> -->
+        <Navbar />
+        <!-- SubNavbar -->
+        <header class="bg-dark text-light shadow py-3" style="margin-top: 60px;" v-if="$slots.header">
+            <div class="container py-6 px-4">
+                <slot name="header" />
+            </div>
+        </header>
 
 
 
-            <!-- Page Content -->
-            <main>
-                <slot name="main" />
-            </main>
-
-        </div>
+        <!-- Page Content -->
+        <main>
+            <slot name="main" />
+        </main>
     </div>
 </template>
