aed4cceb4f85ab8adb3dcdca0ec86bf9a5659d89
Jac22 <jacopo.prudentino@gmail.com>
2023-03-17 09:52:10 +0100

    Given  a layout to transaction Result page

--- resources/js/Pages/Braintree/Result.vue
+++ resources/js/Pages/Braintree/Result.vue
@@ -1,16 +1,37 @@
 <script setup>
 import GuestLayout from '@/Layouts/GuestLayout.vue';
+import { Link } from '@inertiajs/vue3';
 const props = defineProps(['result', 'developer']);
 console.log(props.result);
 console.log(props.developer);
 </script>
 
 <template>
-    <GuestLayout>
-        <div class="my_container bg-dark">
+    <GuestLayout class="bg-dark overflow-hidden">
+        <div class="my_container">
             <template v-if="result.success">
-                <h1>Congratulation!</h1>
+                <h1 class="text-success">Congratulation! You are now sponsored</h1>
+                <div class="d-flex justify-content-center gap-3 pt-5">
+                    <Link :href="route('home')" class="btn btn-light">Go to homepage</Link>
+                    <Link :href="route('braintree')" class="btn btn-light">Buy another sponsor plan</Link>
+                    <Link :href="route('dashboard')" class="btn btn-light">Go to dashborad</Link>
+                </div>
+            </template>
+            <template v-else>
+                <h1 class="text-danger">Transaction failed</h1>
+                <div class="d-flex justify-content-center gap-3 pt-5">
+                    <Link :href="route('home')" class="btn btn-light">Go to homepage</Link>
+                    <Link :href="route('braintree')" class="btn btn-light">Retry</Link>
+                    <Link :href="route('dashboard')" class="btn btn-light">Go to dashborad</Link>
+                </div>
             </template>
         </div>
     </GuestLayout>
 </template>
+
+<style lang="scss" scoped>
+h1{
+    margin-top: 20vh;
+    font-size: calc(1.35rem + 1.5vw);
+}
+</style>
