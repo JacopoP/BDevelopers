eb55d28a28bc7a3ea45226c18d9e96667b92f99b
Jac22 <jacopo.prudentino@gmail.com>
2023-03-21 16:44:44 +0100

    Recharge navbar

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -279,12 +279,14 @@ export default {
     data() {
         return {
             new_profile_path: undefined,
+            reloadNavbar: true,
         }
     },
     methods: {
         updateImg() {
+            this.reloadNavbar=false;
             axios.get('http://localhost:8000/api/v1/profile_path' + usePage().props.auth.user.id)
-                .then((res) => { this.new_profile_path = 'sthourge/' + res.data.response.path })
+                .then((res) => { this.new_profile_path = 'storage/' + res.data.response.path; this.reloadNavbar=true})
                 .catch((error) => console.log(error))
         }
     }
@@ -294,7 +296,7 @@ export default {
 <template>
     <Head title="Dashboard" />
 
-    <AuthenticatedLayout>
+    <AuthenticatedLayout :reloadNavbar="this.reloadNavbar">
 
         <template #header>
             <div class="d-flex justify-content-between">
